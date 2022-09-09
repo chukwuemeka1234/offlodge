e84747cc58d9b51427c75b87a03f71cf887c48e6
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-09-10 00:08:39 +0100

    arrays of links for Nav

--- src/data.js
+++ src/data.js
@@ -1,1 +1,28 @@
 import {SiOpenaigym} from 'react-icons/Si'
+
+export const links = [
+  {
+    name: "Home",
+    path: '/'
+  },
+  {
+    name: "About",
+    path: '/about'
+  },
+  {
+    name: "Gallery",
+    path: '/gallery'
+  },
+  {
+    name: "Plans",
+    path: '/plans'
+  },
+  {
+    name: "Trainers",
+    path: '/trainers'
+  },
+  {
+    name: "Contact",
+    path: '/contact'
+  },
+]
