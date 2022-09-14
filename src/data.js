7fbe73dec84d8c2b1444a6a68f4f0af6332ec010
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-09-14 23:21:01 +0100

    Add data to file

--- src/data.js
+++ src/data.js
@@ -26,3 +26,94 @@ export const links = [
     path: '/contact'
   },
 ]
+
+export const programs = [
+  {
+    id: 1,
+    icon: <SiOpenaigym/>,
+    title: "Program One",
+    info: "This is the beginning of something huge",
+    path: "/programs/111"
+  },
+  {
+    id: 2,
+    icon: <SiOpenaigym/>,
+    title: "Program Two",
+    info: "This is the beginning of something huge",
+    path: "/programs/222"
+  },
+  {
+    id: 3,
+    icon: <SiOpenaigym/>,
+    title: "Program Three",
+    info: "This is the beginning of something huge",
+    path: "/programs/333"
+  },
+  {
+    id: 4,
+    icon: <SiOpenaigym/>,
+    title: "Program Four",
+    info: "This is the beginning of something huge",
+    path: "/programs/444"
+  }
+]
+
+export const values = [
+  {
+    id: 1,
+    icon: <SiOpenaigym/>,
+    title: "Value One",
+    desc: "This are the values that I will need for this project."
+  },
+  {
+    id: 2,
+    icon: <SiOpenaigym/>,
+    title: "Value Two",
+    desc: "This are the values that I will need for this project."
+  },
+  {
+    id: 3,
+    icon: <SiOpenaigym/>,
+    title: "Value Three",
+    desc: "This are the values that I will need for this project."
+  },
+  {
+    id: 4,
+    icon: <SiOpenaigym/>,
+    title: "Value Four",
+    desc: "This are the values that I will need for this project."
+  }
+]
+
+export const faqs = [
+  {
+    id: 1,
+    question: "How often should I exercise?",
+    answer: "You should exercise weekly. Onc or twice in a week to gain much fitness. You can decide to go to the gym and train there."
+  },
+  {
+    id: 2,
+    question: "How often should I exercise?",
+    answer: "You should exercise weekly. Onc or twice in a week to gain much fitness. You can decide to go to the gym and train there."
+  },
+  {
+    id: 3,
+    question: "How often should I exercise?",
+    answer: "You should exercise weekly. Onc or twice in a week to gain much fitness. You can decide to go to the gym and train there."
+  },
+  {
+    id: 4,
+    question: "How often should I exercise?",
+    answer: "You should exercise weekly. Onc or twice in a week to gain much fitness. You can decide to go to the gym and train there."
+  },
+  {
+    id: 5,
+    question: "How often should I exercise?",
+    answer: "You should exercise weekly. Onc or twice in a week to gain much fitness. You can decide to go to the gym and train there."
+  },
+  {
+    id: 6,
+    question: "How often should I exercise?",
+    answer: "You should exercise weekly. Onc or twice in a week to gain much fitness. You can decide to go to the gym and train there."
+  }
+]
