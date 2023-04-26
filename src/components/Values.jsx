f7fe00598a057efdfc169d548174b9d2bbc224a2
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-04-26 23:49:28 +0100

    Working on values section

---
+++ src/components/Values.jsx
@@ -0,0 +1,21 @@
+import Image from '../images/values-img.jpg'
+import SectionHead from './SectionHead'
+import {GitCutDiamond} from 'react-icons/gi'
+import { values } from '../data'
+import Card from '../UI/Card'
+
+const Values = () => {
+  return (
+    <section className='values'>
+      <div className="container values__container">
+        <div className="values__left">
+          <div className="values__image">
+            <img src={Image} alt="Values Image" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Values
