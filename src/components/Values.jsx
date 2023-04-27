3c7ebbf2497ec05486e559fdfa52de347913a04a
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-04-27 23:31:22 +0100

    Add styling for values section

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,6 +1,6 @@
 import Image from '../images/values-img.jpg'
 import SectionHead from './SectionHead'
-import {GitCutDiamond} from 'react-icons/gi'
+import {GiDiamondTrophy} from 'react-icons/gi'
 import { values } from '../data'
 import Card from '../UI/Card'
 
@@ -10,7 +10,23 @@ const Values = () => {
       <div className="container values__container">
         <div className="values__left">
           <div className="values__image">
-            <img src={Image} alt="Values Image" />
+            <img src={Image} alt="Values image" />
+          </div>
+        </div>
+        <div className="values__right">
+          <SectionHead icon={<GiDiamondTrophy/>} title="Values"/>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis ratione aliquam quidem voluptatem delectus?
+          </p>
+          <div className="values__wrapper">
+            {
+              values.map(({id, icon, title, desc}) => {
+                return <Card className="values__value">
+                      <span>{icon}</span>
+                      <h4>{title}</h4>
+                      <small>{desc}</small>
+                </Card>
+              })
+            }
           </div>
         </div>
       </div>
