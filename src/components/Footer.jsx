743fd0454a016c1c0d42c6959aa8d275d0911e6b
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-05-11 22:37:52 +0100

    Add the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,43 @@ const Footer = () => {
   return (
     <footer>
       <div className="container footer__container">
-        <article></article>
+        <article>
+          <Link to="/" className='logo-footer'>
+            <img src={Logo} alt="Footer Logo" />
+          </Link>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorum at excepturi laboriosam placeat rem. Natus ducimus, maiores recusandae rerum numquam, necessitatibus temporibus minima dolorum animi similique nesciunt laudantium quibusdam.
+          </p>
+          <div className="footer__socials">
+            <a href="https://linkedin.com" target="blank" rel='noreferrer noopener'><FaLinkedin/></a>
+            <a href="https://twitter.com" target="blank" rel='noreferrer noopener'><AiOutlineTwitter/></a>
+            <a href="https://facebook.com" target="blank" rel='noreferrer noopener'><FaFacebook/></a>
+            <a href="https://instagram.com" target="blank" rel='noreferrer noopener'><AiOutlineInstagram/></a>
+          </div>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/plans">Plans</Link>
+          <Link to="/trainers">Trainers</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+        <article>
+          <h4>Insights</h4>
+          <Link to="/s">Blog</Link>
+          <Link to="/s">Case Studies</Link>
+          <Link to="/s">Events</Link>
+          <Link to="/s">Communities</Link>
+          <Link to="/s">FAQs</Link>
+        </article>
+        <article>
+          <h4>Get In Touch</h4>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/s">Support</Link>
+        </article>
+      </div>
+      <div className="footer__copyright">
+        <small>2023 VIVALDI CODE &copy; All Rights Reserved</small>
       </div>
     </footer>
   )
