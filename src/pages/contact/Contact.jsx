689a88a94be87316dc1dda82558b2741de9796ba
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-05-25 13:24:36 +0100

    Add contact section media queries

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,27 @@
+import Header from '../../components/Header';
+import HeaderImage from '../../images/main-header-img.jpg'
+import {MdEmail} from 'react-icons/md'
+import {BsMessenger} from 'react-icons/bs'
+import {IoLogoWhatsapp} from 'react-icons/io'
+
 import './contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Header title="Get in touch" image={HeaderImage}>
+    Architecto at perspiciatis repellat aut vero quaerat odit quam quis voluptate eaque ullam veniam omnis, possimus id rerum? Recusandae autem atque cumque.
+    </Header>
+    <section className="contact">
+      <div className="container contact__container">
+        <div className="contact__wrapper">
+          <a href="mailto:chimdiebubev@gmail.com" target="_blank" rel="noreferrer"><MdEmail/></a>
+          <a href="http://m.me/ani.victor.chukwuemeka" target="_blank" rel="noreferrer"><BsMessenger/></a>
+          <a href="https://wa.me/+2347065127989" target="_blank" rel="noreferrer"><IoLogoWhatsapp/></a>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
