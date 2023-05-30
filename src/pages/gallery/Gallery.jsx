ca5ea5f3224f038c6f171099636bbb817d19895b
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-05-30 14:37:27 +0100

    Add gallery pictures and styling the media queries

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,8 +1,32 @@
+import Header from '../../components/Header';
+import HeaderImage from '../../images/Gallery-img1.jpg'
 import './gallery.css';
 
 const Gallery = () => {
+  const galleryLength = 15;
+  const images = []
+
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`))
+  }
+
   return (
-    <div>Gallery</div>
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui modi esse nisi et, autem vitae placeat ab dolor rerum eveniet praesentium ipsam consectetur incidunt. Animi itaque non consequatur eos consectetur!
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+        {
+          images.map((image, index) => {
+            return <article key={index}>
+              <img src={image} alt={`Gallery Image ${index + 1}`} />
+            </article>
+          })
+        }
+        </div>
+      </section>
+    </>
   )
 }
 
